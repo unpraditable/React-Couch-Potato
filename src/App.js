cbd74d1610bdfed18dc28a1e374ec4a510f6e6f1
Aditya Pradita <pradita397@gmail.com>
2020-03-30 22:12:48 +0700

    Add routing for popular and trending movies

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home.js';
 import Header from './components/Header.js';
 import MovieDetail from './pages/MovieDetail';
 import ActorDetail from './pages/ActorDetail';
+import MovieCard from './components/MovieCard';
 
 import {
   BrowserRouter as Router,
@@ -19,17 +20,29 @@ function App() {
       <div className="App">
         <Header />
         <div className="App-body">
+
+          {/* I use normal routing here */}
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/popular">
+            <div className="container">
+              <MovieCard type="top_rated"/>
+            </div>
+          </Route>
+          <Route exact path="/trending">
+            <div className="container">
+              <MovieCard type="popular"/>
+            </div>
+          </Route>
+
+          {/* I use switch for nested routes, so those components will be rendered inclusively and it will be helpful for nested URLs */}
           <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route path="/movies/:movieSlug" component={MovieDetail}>
-              </Route>
-              <Route path="/casts/:castSlug" component={ActorDetail}>
-              </Route>
-            </Switch>
-            {/* <MovieDetail /> */}
-            {/* <ActorDetail /> */}
+            <Route path="/movies/:movieSlug" component= {MovieDetail}>
+            </Route>
+            <Route path="/casts/:castSlug" component={ActorDetail}>
+            </Route>
+          </Switch>
         </div>
       </div>
     </Router>
